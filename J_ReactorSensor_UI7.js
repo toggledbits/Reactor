97efecbff3d8d10d7595fb7d856999810cad298d
Patrick Rigney <patrick@toggledbits.com>
2019-04-12 21:16:29 -0400

    Make sure evenlist always shows

--- J_ReactorSensor_UI7.js
+++ J_ReactorSensor_UI7.js
@@ -2138,7 +2138,7 @@ if ( ctx === "tab-conds" ) CondBuilder.redraw( myid );
 			jQuery("select.varmenu", $row).off( 'change.reactor' ).on( 'change.reactor', handleConditionVarChange );
 
 			newMenu = makeEventMenu( cond, $row );
-			jQuery( 'select#eventlist', $row ).replaceWith( newMenu );
+			jQuery( 'div#eventlist', $row ).replaceWith( newMenu );
 
 			updateCurrentServiceValue( $row );
 
@@ -2307,65 +2307,67 @@ if ( ctx === "tab-conds" ) CondBuilder.redraw( myid );
 		 */
 		function makeEventMenu( cond, $row ) {
 			var dobj = api.getDeviceObject( cond.device );
-			if ( ! dobj ) return;
-			var url = api.getDataRequestURL().replace( /\/data_request.*$/, "/" );
-			jQuery.ajax({
-				url: url + dobj.device_json,
-				dataType: "json",
-				timeout: 5000
-			}).done( function( data, statusText, jqXHR ) {
-				var first = true;
-				var mm = jQuery( 'div#eventlist ul.dropdown-menu', $row ).empty();
-				if ( data && data.eventList2 ) {
-					var wrapAction = function( eventinfo, cond, $row ) {
-						return function( ev ) {
-							var el = jQuery( ev.target );
-							if ( eventinfo.serviceStateTable ) {
-								cond.service = eventinfo.serviceId;
-								cond.variable = firstKey( eventinfo.serviceStateTable );
-								cond.operator = eventinfo.serviceStateTable[cond.variable].comparisson || "=";
-								cond.value = eventinfo.serviceStateTable[cond.variable].value || "";
-							} else if ( eventinfo.argumentList ) {
-								cond.service = eventinfo.serviceId;
-								cond.variable = eventinfo.argumentList[0].name;
-								cond.operator = eventinfo.argumentList[0].comparisson || "=";
-								cond.value = eventinfo.argumentList[0].defaultValue || "";
-							}
-							var sk = cond.service + "/" + cond.variable;
-							if ( 0 === jQuery( 'select.varmenu option[value="' + idSelector( sk ) + '"]', $row ).length ) {
-								jQuery( 'select.varmenu', $row ).append( jQuery( '<option/>').val( sk ).text( sk ) );
-							}
-							jQuery( 'select.varmenu', $row ).val( sk );
-							jQuery( 'select.opmenu', $row ).val( cond.operator );
-							jQuery( 'input#value', $row ).val( cond.value );
-							configModified = true;
-							setUpConditionOpFields( $row, cond );
-							updateCurrentServiceValue( $row );
-							updateConditionRow( $row, jQuery( ev ) );
+			if ( dobj ) {
+				var url = api.getDataRequestURL().replace( /\/data_request.*$/, "/" );
+				jQuery.ajax({
+					url: url + dobj.device_json,
+					dataType: "json",
+					timeout: 5000
+				}).done( function( data, statusText, jqXHR ) {
+					var first = true;
+					var mm = jQuery( 'div#eventlist ul.dropdown-menu', $row ).empty();
+					if ( data && data.eventList2 ) {
+						var wrapAction = function( eventinfo, cond, $row ) {
+							return function( ev ) {
+								var el = jQuery( ev.target );
+								if ( eventinfo.serviceStateTable ) {
+									cond.service = eventinfo.serviceId;
+									cond.variable = firstKey( eventinfo.serviceStateTable );
+									cond.operator = eventinfo.serviceStateTable[cond.variable].comparisson || "=";
+									cond.value = eventinfo.serviceStateTable[cond.variable].value || "";
+								} else if ( eventinfo.argumentList ) {
+									cond.service = eventinfo.serviceId;
+									cond.variable = eventinfo.argumentList[0].name;
+									cond.operator = eventinfo.argumentList[0].comparisson || "=";
+									cond.value = eventinfo.argumentList[0].defaultValue || "";
+								}
+								var sk = cond.service + "/" + cond.variable;
+								if ( 0 === jQuery( 'select.varmenu option[value="' + idSelector( sk ) + '"]', $row ).length ) {
+									jQuery( 'select.varmenu', $row ).append( jQuery( '<option/>').val( sk ).text( sk ) );
+								}
+								jQuery( 'select.varmenu', $row ).val( sk );
+								jQuery( 'select.opmenu', $row ).val( cond.operator );
+								jQuery( 'input#value', $row ).val( cond.value );
+								configModified = true;
+								setUpConditionOpFields( $row, cond );
+								updateCurrentServiceValue( $row );
+								updateConditionRow( $row, jQuery( ev ) );
+							};
 						};
-					};
-					for ( var ix=0; ix<data.eventList2.length; ix++ ) {
-						var cx = data.eventList2[ix];
-						var txt = ((cx.label || {}).text || String(cx.id)).replace( /_DEVICE_NAME_/g, "device" );
-						var lk = jQuery( '<a href="#"></a>' ).text( txt );
-						lk.attr( 'id', cx.id );
-						lk.on( 'click.reactor', wrapAction( cx, cond, $row ) );
-						mm.append( jQuery( '<li />' ).append( lk ) );
+						for ( var ix=0; ix<data.eventList2.length; ix++ ) {
+							var cx = data.eventList2[ix];
+							var txt = ((cx.label || {}).text || String(cx.id)).replace( /_DEVICE_NAME_/g, "device" );
+							var lk = jQuery( '<a href="#"></a>' ).text( txt );
+							lk.attr( 'id', cx.id );
+							lk.on( 'click.reactor', wrapAction( cx, cond, $row ) );
+							mm.append( jQuery( '<li />' ).append( lk ) );
+						}
 					}
-				}
-				if ( jQuery( 'li', mm ).length > 0 ) {
-					mm.append( jQuery( '<li class="divider" />' ) );
-					mm.append( jQuery( '<li />' )
-						.text( "In addition to the above device-defined conditions, you can select any state variable defined on the device and test it." ) );
-					jQuery( 'div#eventlist', $row ).show().prop( 'disabled', false );
-				} else {
-					jQuery( 'div#eventlist', $row ).prop( 'disabled', false ).hide();
-				}
-			}).fail( function( jqXHR ) {
-				console.log("Failed to retrieve and parse " + dobj.device_json);
-			});
+					if ( jQuery( 'li', mm ).length > 0 ) {
+						mm.append( jQuery( '<li class="divider" />' ) );
+						mm.append( jQuery( '<li />' )
+							.text( "In addition to the above device-defined conditions, you can select any state variable defined on the device and test it." ) );
+						jQuery( 'div#eventlist', $row ).show().prop( 'disabled', false );
+					} else {
+						jQuery( 'div#eventlist', $row ).prop( 'disabled', false ).hide();
+					}
+				}).fail( function( jqXHR ) {
+					console.log("Failed to retrieve and parse " + dobj.device_json);
+				});
+			}
+
 			var el = jQuery( '<div id="eventlist" class="dropdown" />' ).prop('disabled', true).hide();
-			el.append( '<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" title="Click for device-defined events"><i class="material-icons md14">chevron_right</i></button>' );
+			el.append( '<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" title="Click for device-defined events"><i class="material-icons">chevron_right</i></button>' );
 			el.append( '<ul class="dropdown-menu" role="menu" />' );
 			return el;
 		}
