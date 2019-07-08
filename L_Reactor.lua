b68b51078fd464de0aef02cb39e86bc73d08527b
Patrick Rigney <patrick@toggledbits.com>
2019-07-08 19:56:53 -0400

    Always watch state var for group state cond

--- L_Reactor.lua
+++ L_Reactor.lua
@@ -2304,12 +2304,15 @@ local function evaluateCondition( cond, grp, cdata, tdev ) -- luacheck: ignore 2
 		end
 
 		-- Get group state value; use cstate if local
+		local varname = string.format( "GroupStatus_%s", cond.groupid or "?" )
+		-- Add service watch if we don't have one.
+		addServiceWatch( devnum, GRPSID, varname, tdev )
+
 		local vv
 		if devnum == tdev then
 			local cs = sst.condState[cond.groupid] or {}
 			vv = cs.evalstate or false
 		else
-			local varname = string.format( "GroupStatus_%s", cond.groupid or "?" )
 			vv = getVarNumeric( varname, -1, devnum, GRPSID )
 
 			-- Boolean should come back 0 or 1; if -1, group does not exist or is not ready/available
@@ -2324,9 +2327,6 @@ local function evaluateCondition( cond, grp, cdata, tdev ) -- luacheck: ignore 2
 				return nil,nil
 			end
 			vv = vv ~= 0 -- boolean!
-
-			-- Add service watch if we don't have one.
-			addServiceWatch( devnum, GRPSID, varname, tdev )
 		end
 
 		if cond.operator == "change" then
