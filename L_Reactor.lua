284c6f993d00f320608c86070937db4ee5c3d9fb
Patrick Rigney <patrick@toggledbits.com>
2020-05-07 21:59:24 -0400

    Bump

--- L_Reactor.lua
+++ L_Reactor.lua
@@ -11,7 +11,7 @@ local debugMode = false
 
 local _PLUGIN_ID = 9086
 local _PLUGIN_NAME = "Reactor"
-local _PLUGIN_VERSION = "3.6develop-20127"
+local _PLUGIN_VERSION = "3.6develop-20128"
 local _PLUGIN_URL = "https://www.toggledbits.com/reactor"
 local _DOC_URL = "https://www.toggledbits.com/static/reactor/docs/3.6/"
 
