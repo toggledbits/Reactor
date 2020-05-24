0d219a60305c2af560879398ba35aff5771f8d71
Patrick Rigney <patrick@toggledbits.com>
2020-05-24 09:09:20 -0400

    Bump

--- J_Reactor_UI7.js
+++ J_Reactor_UI7.js
@@ -17,7 +17,7 @@ var Reactor = (function(api, $) {
 	/* unique identifier for this plugin... */
 	var uuid = '72acc6ea-f24d-11e8-bd87-74d4351650de';
 
-	var pluginVersion = '3.7develop-20131';
+	var pluginVersion = '3.7develop-20145';
 
 	var _UIVERSION = 20130;     /* must coincide with Lua core */
 
