5561bc71e23ee458c6e4c873e7f7e6ba0683d2c4
Patrick Rigney <patrick@toggledbits.com>
2020-12-18 08:56:13 -0500

    Updates for ALTUI update to jQuery and Bootstrap

--- J_ReactorSensor_ALTUI.js
+++ J_ReactorSensor_ALTUI.js
@@ -13,8 +13,7 @@
 var ReactorSensor_ALTUI = ( function( window, undefined ) {
 
 	function _getStyle() {
-		var style = "";
-		style += ".dXelaylight-cpbutton { height: 24px; }";
+		var style = "button.reactor-cpb { padding: .25rem .5rem; min-width: 4rem; }";
 		return style;
 	}
 
@@ -25,21 +24,21 @@ var ReactorSensor_ALTUI = ( function( window, undefined ) {
 			var armed = MultiBox.getStatus( device, "urn:micasaverde-com:serviceId:SecuritySensor1", "Armed");
 			html += '<div class="pull-right">';
 			html += ALTUI_PluginDisplays.createOnOffButton( enab, "reactor-enabled-" + device.altuiid, _T("Disabled,Enabled"), "pull-right");
-			html += ALTUI_PluginDisplays.createOnOffButton( armed, "reactor-armed-" + device.altuiid, _T("Disarmed,Armed"), "pull-right");
+			// html += ALTUI_PluginDisplays.createOnOffButton( armed, "reactor-armed-" + device.altuiid, _T("Disarmed,Armed"), "pull-right");
 			html += '</div>';
 			html += '<div>' + message + '</div>';
 			html += '<div class="clearfix"></div>';
 			html += '<div>';
-			html += ('<button class="btn-xs btn-default reactor-cpbutton" id="reactor-restart-{0}">'+_T("Restart")+'</button>').format(device.altuiid);
-			html += ('<button class="btn-xs btn-default reactor-cpbutton" id="reactor-reset-{0}">'+_T("Reset")+'</button>').format(device.altuiid);
-			html += ('<button class="btn-xs btn-default reactor-cpbutton" id="reactor-trip-{0}">'+_T("Trip")+'</button>').format(device.altuiid);
+			html += ('<button class="btn btn-xs btn-outline-primary reactor-cpb ml-1" id="reactor-reset-{0}">'+_T("Reset")+'</button>').format(device.altuiid);
+			html += ('<button class="btn btn-xs btn-outline-primary reactor-cpb ml-1" id="reactor-trip-{0}">'+_T("Trip")+'</button>').format(device.altuiid);
+			html += ('<button class="btn btn-xs btn-outline-warning reactor-cpb ml-1" id="reactor-restart-{0}">'+_T("Restart")+'</button>').format(device.altuiid);
 			html += '</div>';
 			html += '<script type="text/javascript">';
 			html += '$("button#reactor-restart-{0}").on("click", function() { ReactorSensor_ALTUI._deviceAction("{0}", "Restart"); } );'.format(device.altuiid);
 			html += '$("button#reactor-reset-{0}").on("click", function() { ReactorSensor_ALTUI._deviceAction("{0}", "Reset"); } );'.format(device.altuiid);
 			html += '$("button#reactor-trip-{0}").on("click", function() { ReactorSensor_ALTUI._deviceAction("{0}", "Trip"); } );'.format(device.altuiid);
 			html += "$('div#reactor-enabled-{0}').on('click', function() { ReactorSensor_ALTUI.toggleEnabled('{0}','div#reactor-enabled-{0}'); } );".format(device.altuiid);
-			html += "$('div#reactor-armed-{0}').on('click', function() { ReactorSensor_ALTUI.toggleArmed('{0}','div#reactor-armed-{0}'); } );".format(device.altuiid);
+			// html += "$('div#reactor-armed-{0}').on('click', function() { ReactorSensor_ALTUI.toggleArmed('{0}','div#reactor-armed-{0}'); } );".format(device.altuiid);
 			html += '</script>';
 			return html;
 	}
@@ -56,11 +55,13 @@ var ReactorSensor_ALTUI = ( function( window, undefined ) {
 					MultiBox.runActionByAltuiID( altuiid, 'urn:toggledbits-com:serviceId:ReactorSensor', 'SetEnabled', {newEnabledValue:newval} );
 			});
 		},
+        /*
 		toggleArmed: function (altuiid, htmlid) {
 			ALTUI_PluginDisplays.toggleButton(altuiid, htmlid, 'urn:micasaverde-com:serviceId:SecuritySensor1', 'Armed', function(id, newval) {
 					MultiBox.runActionByAltuiID( altuiid, 'urn:micasaverde-com:serviceId:SecuritySensor1', 'SetArmed', {newArmedValue:newval} );
 			});
 		},
+        */
 		/* true exports */
 		deviceDraw: _draw,
 		getStyle: _getStyle
